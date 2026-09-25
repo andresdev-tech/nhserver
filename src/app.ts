aeab172dd51a32000e3ec870cd7473d1f85331f9
SrAndres6 <andresrjramillo671@gmail.com>
2026-09-24 22:33:43 -0500

    feat: agregar exportaciones de módulos y tipos en app.ts

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+export {
+    nh_server,
+} from "./core/nh_server.js";
+
+export {
+    nh_request,
+} from "./http/request.js";
+
+export {
+    nh_response,
+} from "./http/response.js";
+
+export {
+    http_server,
+} from "./http/server.js";
+
+export {
+    router,
+} from "./router/router.js";
+
+export {
+    route,
+} from "./router/route.js";
+
+export {
+    rest_adapter,
+} from "./adapter/rest.js";
+
+export type {
+    api_mode,
+} from "./core/nh_server.js";
+
+export type {
+    http_handler,
+} from "./http/server.js";
+
+export type {
+    http_method,
+} from "./router/route.js";
